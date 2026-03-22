feaf23a58eeb0b1a7f233d66d900f948067acded
TonyStark0111 <yedhuku2002@gmail.com>
2026-03-22 13:15:46 +0530

    Update config.py
    Ji

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "testing")
     BOT_OWNER = int(environ.get("BOT_OWNER", "8365451390"))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    # New flag – set to True to enable link removal and slow forwarding
+    LINK_REMOVE_FORWD = environ.get("LINK_REMOVE_FORWD", "True") == "True"
+
 
 class temp(object): 
     lock = {}
@@ -23,11 +23,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
-
-
